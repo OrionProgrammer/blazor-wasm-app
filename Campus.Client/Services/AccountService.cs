b4de1a9a8863e24e718c2e1ab0c051cc04af2931
OrionProgrammer <asheenk@gmail.com>
2024-08-17 09:00:36 +0200

    Added antiforgery toke to http requests

--- Campus.Client/Services/AccountService.cs
+++ Campus.Client/Services/AccountService.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 using Campus.Client.Services.Interfaces;
 using Campus.Model;
 using Newtonsoft.Json;
@@ -10,14 +11,25 @@ namespace Campus.Client.Services
     public class AccountService : IAccountService
     {
         private readonly HttpClient _httpClient;
-
-        public AccountService(HttpClient httpClient)
+        private readonly ISecurityService _securityService;
+        public AccountService(HttpClient httpClient,
+                              ISecurityService securityService)
         {
             _httpClient = httpClient;
+            _securityService = securityService;
+        }
+
+        //Method inlined to be faster
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private async void AddAntiforgeryToken()
+        {
+            _httpClient.DefaultRequestHeaders.Add("X-CSRF-TOKEN", await _securityService.GetAntiforgeryToken());
         }
 
         public async Task<UserSessionModel> Login(LoginModel loginModel)
         {
+            AddAntiforgeryToken();
+
             var response = await _httpClient.PostAsJsonAsync("account/login", loginModel);
             var returnValue = await response.Content.ReadAsStringAsync();
             UserSessionModel studentModel = JsonConvert.DeserializeObject<UserSessionModel>(returnValue);
@@ -27,6 +39,8 @@ namespace Campus.Client.Services
 
         public async Task<bool> Register(StudentModel studentModel)
         {
+            AddAntiforgeryToken();
+
             var response = await _httpClient.PostAsJsonAsync("account/register", studentModel);
 
             return response.IsSuccessStatusCode;
--- Campus.Client/Services/StudentService.cs
+++ Campus.Client/Services/StudentService.cs
@@ -3,7 +3,10 @@ using Campus.Client.Helpers;
 using Campus.Client.Services.Interfaces;
 using Campus.Model;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 
 namespace Campus.Client.Services
 {
@@ -11,22 +14,36 @@ namespace Campus.Client.Services
     {
         private readonly HttpClient _httpClient;
         private readonly ISessionStorageService _sessionStorage;
+        private readonly ISecurityService _securityService;
 
         public StudentService(HttpClient httpClient,
-                              ISessionStorageService sessionStorage)
+                              ISessionStorageService sessionStorage,
+                              ISecurityService securityService)
         {
             _httpClient = httpClient;
+            _securityService = securityService;
             _sessionStorage = sessionStorage;
+        }
+
+        //Method inlined to be faster
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private async void AddAntiforgeryToken()
+        {
+            _httpClient.DefaultRequestHeaders.Add("X-CSRF-TOKEN", await _securityService.GetAntiforgeryToken());
         }
 
         public async Task<bool> DeRegister(int courseId)
         {
+            AddAntiforgeryToken();
+
             var response = await _httpClient.DeleteAsync($"student/{courseId}");
             return response.IsSuccessStatusCode;
         }
 
         public async Task<List<StudentCourseModel>> FetchMyCourses(string studentId)
         {
+            AddAntiforgeryToken();
+
             var entities = new List<StudentCourseModel>();
 
             var response = await _httpClient.GetAsync($"student/list/{studentId}");
@@ -42,6 +59,8 @@ namespace Campus.Client.Services
 
         public async Task<bool> Register(StudentCourseModel studentCourseModel)
         {
+            AddAntiforgeryToken();
+
             var response = await _httpClient.PostAsJsonAsync("student/register", studentCourseModel);
             return response.IsSuccessStatusCode;
         }
